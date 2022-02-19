a2f1bf3aad4e307178df4d2e5a8f28d9169dfe83
ObsoleteXero <51947152+ObsoleteXero@users.noreply.github.com>
2022-02-20 01:52:18 +0530

    improved progress display

--- main.py
+++ main.py
@@ -8,109 +8,127 @@ def main():
         return
 
     # TODO: Check inputs
-    length = get_length(sys.argv[1])
     # TODO: Filesize formats
-    filesize = float(sys.argv[2])
+    # TODO: Check OS
+    filename = sys.argv[1]
+    filesize = int(sys.argv[2])
 
-    br = calculate_bitrate(filesize, length)
-    x264(sys.argv[1], br)
-
-
-def get_length(filename: str) -> float:
-    result = subprocess.run(
-        [
-            "ffprobe",
-            "-v",
-            "error",
-            "-show_entries",
-            "format=duration",
-            "-of",
-            "default=noprint_wrappers=1:nokey=1",
-            filename,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
-    return float(result.stdout)
+    ffcmd = Compress(filename, filesize)
+    ffcmd.x264()
 
 
-def calculate_bitrate(filesize: float, length: float) -> str:
-    v_br = round(filesize * 1024 * 8 / length - 128)
-    return str(v_br) + "k"
+class Compress:
+    def __init__(self, filename, target_size) -> None:
+        self.filename = filename
+        self.target_size = target_size
 
+    def get_info(self) -> None:
+        print("Gathering information...", end="\r")
+        result = subprocess.run(
+            [
+                "ffprobe",
+                "-count_frames",
+                "-select_streams",
+                "v:0",
+                "-v",
+                "error",
+                "-show_entries",
+                "stream=nb_read_frames : format=duration",
+                "-of",
+                "default=noprint_wrappers=1:nokey=1",
+                self.filename,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        print("Gathering information... Complete")
+        frames, length = result.stdout.strip().splitlines()
+        self.frames = int(frames)
+        self.length = float(length)
 
-def x265():
-    # TODO: x265
-    return
+    def calculate_bitrate(self) -> None:
+        v_br = round(self.target_size * 1024 * 8 / self.length - 128)
+        self.br = str(v_br) + "k"
 
+    def x264(self) -> None:
 
-def x264(filename: str, bitrate: str) -> None:
-    pass_one = subprocess.Popen(
-        [
-            "ffmpeg",
-            "-y",
-            "-i",
-            filename,
-            "-c:v",
-            "libx264",
-            "-b:v",
-            bitrate,
-            "-pass",
-            "1",
-            "-an",
-            "-f",
-            "null",
-            "NUL",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
+        self.get_info()
+        self.calculate_bitrate()
 
-    while True:
-        line = pass_one.stdout.readline().decode("utf8", errors="replace").strip()
-        if line == "":
-            print("First Pass: Complete")
-            break
-        if line.startswith('total_size='):
-            progress = round(int(line.lstrip('total_size=')) / (int(sys.argv[2]) * 1048576) * 100, 2)
-            print(f"First Pass: {progress}%", end='\r')
+        pass_one = subprocess.Popen(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                self.filename,
+                "-c:v",
+                "libx264",
+                "-b:v",
+                self.br,
+                "-pass",
+                "1",
+                "-an",
+                "-f",
+                "null",
+                "-progress",
+                "-",
+                "-nostats",
+                "-loglevel",
+                "error",
+                "NUL",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
 
-    pass_two = subprocess.Popen(
-        [
-            "ffmpeg",
-            "-y",
-            "-i",
-            filename,
-            "-c:v",
-            "libx264",
-            "-b:v",
-            bitrate,
-            "-pass",
-            "2",
-            "-c:a",
-            "aac",
-            "-b:a",
-            "128k",
-            "-progress",
-            "-",
-            "-nostats",
-            "-loglevel",
-            "error",
-            "compressed_" + filename,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
+        while True:
+            line = pass_one.stdout.readline().decode("utf8", errors="replace").strip()
+            if line == "":
+                print("First Pass: Complete")
+                break
+            if line.startswith("frame="):
+                progress = round(int(line.lstrip("frame=")) / self.frames * 100, 2)
+                print(f"First Pass: {progress:.2f}%", end="\r")
 
-    while True:
-        line = pass_two.stdout.readline().decode("utf8", errors="replace").strip()
-        if line == "":
-            print("Second Pass: Complete")
-            break
-        if line.startswith('total_size='):
-            progress = round(int(line.lstrip('total_size=')) / (int(sys.argv[2]) * 1048576) * 100, 2)
-            print(f"Second Pass: {progress}\%", end='\r')
+        pass_two = subprocess.Popen(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                self.filename,
+                "-c:v",
+                "libx264",
+                "-b:v",
+                self.br,
+                "-pass",
+                "2",
+                "-c:a",
+                "aac",
+                "-b:a",
+                "128k",
+                "-progress",
+                "-",
+                "-nostats",
+                "-loglevel",
+                "error",
+                "compressed_" + self.filename,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
 
+        while True:
+            line = pass_two.stdout.readline().decode("utf8", errors="replace").strip()
+            if line == "":
+                print("Second Pass: Complete")
+                break
+            if line.startswith("frame="):
+                progress = round(int(line.lstrip("frame=")) / self.frames * 100, 2)
+                print(f"Second Pass: {progress:.2f}%", end="\r")
+
+    def x265():
+        # TODO: x265
+        return
 
 
 if __name__ == "__main__":
